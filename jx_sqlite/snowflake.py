b641bfd051fef041a8ed290b15a88a3abc921b09
Rohit <rohitkumar.a255@gmail.com>
2017-04-10 20:34:01 +0530

    .

--- jx_sqlite/snowflake.py
+++ jx_sqlite/snowflake.py
@@ -23,9 +23,7 @@ class Snowflake(object):
         if not self.read_db():
             self.create_fact(uid)
 
-    def __del__(self):
-        for nested_path, table in self.tables.items():
-            self.db.execute("DROP TABLE " + quote_table(concat_field(self.fact, nested_path)))
+   
 
     def read_db(self):
         """
