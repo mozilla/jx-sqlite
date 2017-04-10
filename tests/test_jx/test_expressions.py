c9f9a9302d57ff5ea72bcbe3748bb5b91a8b9ff6
Rohit <rohitkumar.a255@gmail.com>
2017-04-10 20:32:16 +0530

    started creating module library

--- tests/test_jx/test_expressions.py
+++ tests/test_jx/test_expressions.py
@@ -11,7 +11,7 @@
 from __future__ import division
 from __future__ import unicode_literals
 
-from pyLibrary.queries.domains import is_variable_name
+from library.queries import is_variable_name
 from pyLibrary.queries.expressions import simplify_esfilter, jx_expression, USE_BOOL_MUST
 from mo_testing.fuzzytestcase import FuzzyTestCase
 from mo_times.dates import Date
