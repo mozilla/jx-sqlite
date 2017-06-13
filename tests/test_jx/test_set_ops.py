f968fba2e68d99bf7192415604d11d0185bc8148
Rohit <rohitkumar.a255@gmail.com>
2017-06-14 02:23:30 +0530

    test change for table type

--- tests/test_jx/test_set_ops.py
+++ tests/test_jx/test_set_ops.py
@@ -1107,8 +1107,8 @@ class TestSetOps(BaseTestCase):
                 "meta": {"format": "table"},
                 "header": ["_a"],
                 "data": [
-                   [{"b": 1, "c": 1}, {"b": 2, "c": 1}],
-                   [{"b": 1, "c": 2}, {"b": 2, "c": 2}]
+                   [[{"b": 1, "c": 1}, {"b": 2, "c": 1}]],
+                   [[{"b": 1, "c": 2}, {"b": 2, "c": 2}]]
                 ]
             },
             "expecting_cube": {
