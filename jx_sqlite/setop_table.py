1ac12de17f377e780c2866e9bc9626f7f70a68f0
Rohit <rohitkumar.a255@gmail.com>
2017-06-27 15:28:57 +0530

    table format

--- jx_sqlite/setop_table.py
+++ jx_sqlite/setop_table.py
@@ -431,17 +431,17 @@ class SetOpTable(InsertTable):
                 for c in cols:
                     column_names[c.push_column] = c.push_column_name
                 
-                temp_data = Data()
+                temp_data = []
                 for rownum, d in enumerate(data):                
-                    for k, v in d.items(): 
-                        if temp_data[k] == None:
-                            temp_data[k] = [None] * num_rows                        
-                        temp_data[k][rownum] = v
-                       
+                    row = []
+                    for c in cols:
+                        row.append(d[c.push_column_name])
+                    temp_data.append(row)
+
                 return Data(
                     meta={"format": "table"},
                     header=column_names,
-                    data=temp_data
+                    data=[d for d in temp_data]
                 )
             else:
                 column_names = listwrap(query.select).name
