e8d905ca0df2274533a9183d77f166f0155eafd6
Rohit <rohitkumar.a255@gmail.com>
2017-07-13 13:41:50 +0530

    remove_col_from_db

--- jx_sqlite/insert_table.py
+++ jx_sqlite/insert_table.py
@@ -346,7 +346,23 @@ class InsertTable(BaseTable):
                 for i, c in enumerate(self.sf.columns):
                     if c.es_column==d.es_column and c.es_index==d.es_index:
                         del_column.append(i)
+                        self.remove_col_from_db(d)
                 for i in del_column:
                     del(self.sf.columns[i])
                     
                     
+    def remove_col_from_db(self, col):
+        org_table = col.es_index
+        column = col.es_column
+        tmp_table = "tmp_" + col.es_index
+        columns = self.db.query("select * from " + quote_table(org_table)).header
+        self.db.execute(
+            "ALTER TABLE " + quote_table(org_table) + 
+            " RENAME TO " + quote_table(tmp_table)
+        )
+        self.db.execute(
+            "CREATE TABLE " + quote_table(org_table) +
+            " AS SELECT " + (", ".join([quote_table(c) for c in columns if c!=column])) +
+            " FROM " + quote_table(tmp_table)
+        )
+        self.db.execute("DROP TABLE " + quote_table(tmp_table))
