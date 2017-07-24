cca5eb6908d49d9ddc02973d066ec418154b0583
Rohit <rohitkumar.a255@gmail.com>
2017-07-25 03:42:18 +0530

    dequote

--- jx_base/queries.py
+++ jx_base/queries.py
@@ -28,3 +28,13 @@ def is_variable_name(value):
     if not match:
         return False
     return match.group(0) == value
+
+def dequote(s):
+    """
+    If a string has single or double quotes around it, remove them.
+    Make sure the pair of quotes match.
+    If a matching pair of quotes is not found, return the string unchanged.
+    """
+    if (s[0] == s[-1]) and s.startswith(("'", '"')):
+        return s[1:-1]
+    return s
